7f9cb796685bcf5acc71dae42e7f700b4ddbbfbd
A7medBot <105971100+A7medBot@users.noreply.github.com>
2022-05-31 17:52:20 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -62,14 +62,14 @@ global.thumb = fs.readFileSync('./image/chika.jpg')
 
 //—————「 Set Image Welcome 」—————//
 //terserah mau ubah apa nggak, menurut gua jangan di ubah!
-global.BgWelcLeav = 'https://telegra.ph/file/ca207893ae26d531cd9c6.jpg'
+global.BgWelcLeav = 'https://telegra.ph/file/c1413efe285cd9c16a913.jpg'
 
 //—————「 Set Random Image Menu 」—————//
 //gausah di apa² in!
-global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
-global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
-global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
-global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flaming = 'https://telegra.ph/file/c1413efe285cd9c16a913.jpg'
+global.fluming = 'https://telegra.ph/file/c1413efe285cd9c16a913.jpg'
+global.flarun = 'https://telegra.ph/file/c1413efe285cd9c16a913.jpg'
+global.flasmurf = 'https://telegra.ph/file/c1413efe285cd9c16a913.jpg'
 
 //—————「 Set Prefix 」—————//
 //gausah di apa² in!
